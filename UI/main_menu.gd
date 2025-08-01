a080e528ac74791787f1d374a5677e36cac475cd
eddysanoli <eddysanoli@gmail.com>
2025-08-01 03:48:17 -0600

    Menu UI + Main Scene
    Finally got a gist on how menus sort of work. They can be thought of as using flex on web? Made a really crude mockup of a main menu, with options, functional start and quit, with a main scene connected to the "start" button.

---
+++ UI/main_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_play_pressed() -> void:
+    get_tree().change_scene_to_file("res://main.tscn")
+
+func _on_options_pressed() -> void:
+    get_tree().change_scene_to_file("res://UI/options_menu.tscn")
+
+func _on_exit_pressed() -> void:
+    get_tree().quit()
