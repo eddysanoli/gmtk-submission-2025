329eaa3159c859a5f8c689744104375bec721ca2
Alejandro <windevoxhelb@gmail.com>
2025-08-01 23:18:43 -0600

    Stopwatch Function
    Created the stopwatch function that allows to keep the time that it takes the player to complete each lap around the course by using the delta value inside the _process function and saving each time in an array called lap_time which can be later accesed to take the sum of all values and calculate the total time.

--- World/test_world.gd
+++ World/test_world.gd
@@ -1,18 +1,30 @@
 extends Node3D
 
 var anomaly_present = false
+var race_started = false
 var lap_number: int = 3
 var lap_count = 1
+var time_elapsed = 0.0
+
+var lap_time: Array[float] = []
 
 @export var anomaly_scene: PackedScene = preload("res://Objects/anomaly.tscn")
 @onready var path_follow: PathFollow3D = $Path3D/PathFollow3D
 @onready var anomaly_lap = randi_range(2, lap_number)
 
+func _ready() -> void:
+	generate_anomaly()
+
 func _process(delta):
+#	Anomaly Generation
 	if lap_count == anomaly_lap:
 		anomaly_present = true
 		generate_anomaly()
 	
+#	Stopwatch
+	if race_started == true:
+		time_elapsed += delta
+		
 #Checkpoint system
 #Por cada elemento del array generado por el metodo get_children() aplicado al 
 #nodo padre de los checkpoints revisar si se recibe una senal 
@@ -20,8 +32,11 @@ func _process(delta):
 #Una funcion adicional que tome la respuesta del sistema d checkpoints y cruzar la meta
 #para verificar haber completado una vuelta
 
-#Funciones para creear y manejar Timers al comienzo de cada vuelta 
-#basicamente hacer un add_child(Timer)
+#Stopwatch save and restart (call when lap is completed)
+func save_time():
+	var lap_index = lap_count - 1
+	lap_time[lap_index] = time_elapsed
+	time_elapsed = 0.0
 
 func generate_anomaly():
 	var new_anomaly = anomaly_scene.instantiate()
@@ -31,4 +46,3 @@ func generate_anomaly():
 
 func _on_anomaly_anomaly_clear():
 	anomaly_present = false
-	# you can also pick a new lap_target here if you want more spawns
